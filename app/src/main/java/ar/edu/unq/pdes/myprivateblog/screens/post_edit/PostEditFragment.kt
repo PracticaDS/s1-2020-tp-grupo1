04830d82fd1bda1da69d488d99010dd3c47c82aa
camilacintioli <camila.cintioli@gmail.com>
2020-04-11 23:10:24 -0300

    Fix navigation from post detail fragment to post edit fragment

--- app/src/main/java/ar/edu/unq/pdes/myprivateblog/screens/post_edit/PostEditFragment.kt
+++ app/src/main/java/ar/edu/unq/pdes/myprivateblog/screens/post_edit/PostEditFragment.kt
@@ -3,20 +3,33 @@ package ar.edu.unq.pdes.myprivateblog.screens.post_edit
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import ar.edu.unq.pdes.myprivateblog.BaseFragment
+import ar.edu.unq.pdes.myprivateblog.ColorUtils
 import ar.edu.unq.pdes.myprivateblog.R
+import ar.edu.unq.pdes.myprivateblog.data.BlogEntry
 import kotlinx.android.synthetic.main.fragment_post_edit.*
+import java.io.File
 
 class PostEditFragment : BaseFragment() {
     override val layoutId = R.layout.fragment_post_edit
 
     private val viewModel by viewModels<PostEditViewModel> { viewModelFactory }
 
+    private val args: PostEditFragmentArgs by navArgs()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // TODO!!!
+        viewModel.fetchBlogEntry(args.postId)
+
+        viewModel.post.observe(viewLifecycleOwner, Observer {
+            if (it != null) {
+                renderBlogEntry(it)
+            }
+        })
 
         btn_close.setOnClickListener {
             closeAndGoBack()
@@ -27,4 +40,20 @@ class PostEditFragment : BaseFragment() {
     private fun closeAndGoBack() {
         findNavController().navigateUp()
     }
+
+    fun renderBlogEntry(post: BlogEntry){
+        title.setText(post.title)
+
+        header_background.setBackgroundColor(post.cardColor)
+        applyStatusBarStyle(post.cardColor)
+        title.setTextColor(ColorUtils.findTextColorGivenBackgroundColor(post.cardColor))
+
+        if (post.bodyPath != null && context != null) {
+            val content = File(context?.filesDir, post.bodyPath).readText()
+            body.setText(content)
+        }
+
+    }
+
+
 }
--- app/src/main/java/ar/edu/unq/pdes/myprivateblog/screens/post_edit/PostEditViewModel.kt
+++ app/src/main/java/ar/edu/unq/pdes/myprivateblog/screens/post_edit/PostEditViewModel.kt
@@ -1,8 +1,12 @@
 package ar.edu.unq.pdes.myprivateblog.screens.post_edit
 
 import android.content.Context
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import ar.edu.unq.pdes.myprivateblog.data.BlogEntriesRepository
+import ar.edu.unq.pdes.myprivateblog.data.BlogEntry
+import ar.edu.unq.pdes.myprivateblog.data.EntityID
+import ar.edu.unq.pdes.myprivateblog.rx.RxSchedulers
 import javax.inject.Inject
 
 
@@ -11,6 +15,16 @@ class PostEditViewModel @Inject constructor(
     val context: Context
 ) : ViewModel() {
 
-    //TODO!!!
+    var post = MutableLiveData<BlogEntry?>()
+
+    fun fetchBlogEntry(id: EntityID) {
+
+        val disposable = blogEntriesRepository
+            .fetchById(id)
+            .compose(RxSchedulers.flowableAsync())
+            .subscribe {
+                post.value = it
+            }
+    }
 
 }
