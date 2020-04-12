5b7c219c6180f5acb94caa2fc2babb11d27dc774
Sebastian Gonzalez <sebastian.gonzalez@fravega.com.ar>
2020-04-12 17:10:52 -0300

    Fix: save edited blog entry

--- app/src/main/java/ar/edu/unq/pdes/myprivateblog/screens/post_edit/PostEditFragment.kt
+++ app/src/main/java/ar/edu/unq/pdes/myprivateblog/screens/post_edit/PostEditFragment.kt
@@ -2,6 +2,7 @@ package ar.edu.unq.pdes.myprivateblog.screens.post_edit
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
@@ -27,9 +28,36 @@ class PostEditFragment : BaseFragment() {
 
         viewModel.post.observe(viewLifecycleOwner, Observer {
             if (it != null) {
-                renderBlogEntry(it)
+                onPostChange(it)
             }
         })
+
+        viewModel.cardColor.observe(viewLifecycleOwner, Observer {
+            header_background.setBackgroundColor(it)
+            val itemsColor = ColorUtils.findTextColorGivenBackgroundColor(it)
+            title.setTextColor(itemsColor)
+            title.setHintTextColor(itemsColor)
+            btn_save.setColorFilter(itemsColor)
+            btn_close.setColorFilter(itemsColor)
+
+            applyStatusBarStyle(it)
+        })
+
+        title.doOnTextChanged { text, start, count, after ->
+            viewModel.titleText.postValue(text.toString())
+        }
+
+        body.doOnTextChanged { text, start, count, after ->
+            viewModel.bodyText.value = body.toFormattedHtml()
+        }
+
+        btn_save.setOnClickListener {
+            viewModel.savePost()
+        }
+
+        color_picker.onColorSelectionListener = {
+            viewModel.cardColor.postValue(it)
+        }
 
         btn_close.setOnClickListener {
             closeAndGoBack()
@@ -37,23 +65,28 @@ class PostEditFragment : BaseFragment() {
 
     }
 
+    private fun onPostChange(blogEntry: BlogEntry) {
+        val title = blogEntry.title
+        val cardColor = blogEntry.cardColor
+        val body = getBody(blogEntry)
+
+        viewModel.onPostChange(title, cardColor, body)
+        renderBlogEntry(title, cardColor, body)
+    }
+
+    private fun getBody(blogEntry: BlogEntry): String {
+        return File(context?.filesDir, blogEntry.bodyPath).readText()
+    }
+
+    private fun renderBlogEntry(_title: String, _cardColor: Int, _body: String) {
+        title.setText(_title)
+        header_background.setBackgroundColor(_cardColor)
+        applyStatusBarStyle(_cardColor)
+        title.setTextColor(ColorUtils.findTextColorGivenBackgroundColor(_cardColor))
+        body.setText(_body)
+    }
+
     private fun closeAndGoBack() {
         findNavController().navigateUp()
     }
-
-    fun renderBlogEntry(post: BlogEntry){
-        title.setText(post.title)
-
-        header_background.setBackgroundColor(post.cardColor)
-        applyStatusBarStyle(post.cardColor)
-        title.setTextColor(ColorUtils.findTextColorGivenBackgroundColor(post.cardColor))
-
-        if (post.bodyPath != null && context != null) {
-            val content = File(context?.filesDir, post.bodyPath).readText()
-            body.setText(content)
-        }
-
-    }
-
-
 }
--- app/src/main/java/ar/edu/unq/pdes/myprivateblog/screens/post_edit/PostEditViewModel.kt
+++ app/src/main/java/ar/edu/unq/pdes/myprivateblog/screens/post_edit/PostEditViewModel.kt
@@ -1,12 +1,17 @@
 package ar.edu.unq.pdes.myprivateblog.screens.post_edit
 
 import android.content.Context
+import android.graphics.Color
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import ar.edu.unq.pdes.myprivateblog.data.BlogEntriesRepository
 import ar.edu.unq.pdes.myprivateblog.data.BlogEntry
 import ar.edu.unq.pdes.myprivateblog.data.EntityID
 import ar.edu.unq.pdes.myprivateblog.rx.RxSchedulers
+import ar.edu.unq.pdes.myprivateblog.screens.post_create.PostCreateViewModel
+import io.reactivex.Flowable
+import java.io.OutputStreamWriter
+import java.util.*
 import javax.inject.Inject
 
 
@@ -15,16 +20,54 @@ class PostEditViewModel @Inject constructor(
     val context: Context
 ) : ViewModel() {
 
+    enum class State {
+        EDITING, SUCCESS, ERROR
+    }
+
+    val state = MutableLiveData(State.EDITING)
     var post = MutableLiveData<BlogEntry?>()
+    val titleText = MutableLiveData("")
+    val bodyText = MutableLiveData("")
+    val cardColor = MutableLiveData<Int>(Color.LTGRAY)
 
     fun fetchBlogEntry(id: EntityID) {
-
         val disposable = blogEntriesRepository
             .fetchById(id)
             .compose(RxSchedulers.flowableAsync())
             .subscribe {
                 post.value = it
             }
+    }
+
+    fun savePost() {
+        val disposable = Flowable.fromCallable {
+            val fileName = post.value?.bodyPath
+
+            val outputStreamWriter =
+                OutputStreamWriter(context.openFileOutput(fileName, Context.MODE_PRIVATE))
+
+            outputStreamWriter.use { it.flush(); it.write(bodyText.value) }
+
+            fileName
+
+        }.flatMapSingle {
+            blogEntriesRepository.updateBlogEntry(
+                BlogEntry(
+                    uid = post.value!!.uid,
+                    title = titleText.value.toString(),
+                    bodyPath = it,
+                    cardColor = cardColor.value!!
+                )
+            ).toSingle { it }
+        }.compose(RxSchedulers.flowableAsync()).subscribe {
+            state.value = State.SUCCESS
+        }
+    }
+
+    fun onPostChange(_title: String, _cardColor: Int, _body: String) {
+        titleText.value = _title
+        cardColor.value = _cardColor
+        bodyText.value = _body
     }
 
 }
