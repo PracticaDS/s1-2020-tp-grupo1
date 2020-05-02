a055124aca8b9cdff38575ba862d815c81016a2b
camilacintioli <camila.cintioli@gmail.com>
2020-05-02 19:57:47 -0300

    Add blog entries service

--- app/src/main/java/ar/edu/unq/pdes/myprivateblog/screens/post_create/PostCreateViewModel.kt
+++ app/src/main/java/ar/edu/unq/pdes/myprivateblog/screens/post_create/PostCreateViewModel.kt
@@ -4,17 +4,12 @@ import android.content.Context
 import android.graphics.Color
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import ar.edu.unq.pdes.myprivateblog.data.BlogEntriesRepository
-import ar.edu.unq.pdes.myprivateblog.data.BlogEntry
-import ar.edu.unq.pdes.myprivateblog.rx.RxSchedulers
-import io.reactivex.Flowable
-import java.io.OutputStreamWriter
-import java.util.*
+import ar.edu.unq.pdes.myprivateblog.services.BlogEntriesService
 import javax.inject.Inject
 
 
 class PostCreateViewModel @Inject constructor(
-    val blogEntriesRepository: BlogEntriesRepository,
+    val blogEntriesService: BlogEntriesService,
     val context: Context
 ) : ViewModel() {
 
@@ -31,30 +26,11 @@ class PostCreateViewModel @Inject constructor(
 
     fun createPost() {
         // TODO: extract this to some BlogEntryService or BlogEntryActions or some other super meaningful name...
-
-        val disposable = Flowable.fromCallable {
-
-            val fileName = UUID.randomUUID().toString() + ".body"
-            val outputStreamWriter =
-                OutputStreamWriter(context.openFileOutput(fileName, Context.MODE_PRIVATE))
-            outputStreamWriter.use { it.write(bodyText.value) }
-            fileName
-
-        }.flatMapSingle {
-
-            blogEntriesRepository.createBlogEntry(
-                BlogEntry(
-                    title = titleText.value.toString(),
-                    bodyPath = it,
-                    cardColor = cardColor.value!!
-                )
-            )
-
-        }.compose(RxSchedulers.flowableAsync()).subscribe {
-            post = it.toInt()
-            state.value = State.SUCCESS
-        }
-
+        val disposable=blogEntriesService.createBlogEntry(titleText.value.toString(), bodyText.value.toString(), cardColor.value!!)
+            .subscribe {
+                post = it.toInt()
+                state.value = PostCreateViewModel.State.SUCCESS
+            }
     }
 
 }
---
+++ app/src/main/java/ar/edu/unq/pdes/myprivateblog/services/BlogEntriesService.kt
@@ -0,0 +1,53 @@
+package ar.edu.unq.pdes.myprivateblog.services
+
+import android.content.Context
+import ar.edu.unq.pdes.myprivateblog.BaseApplication
+import ar.edu.unq.pdes.myprivateblog.data.BlogEntriesRepository
+import ar.edu.unq.pdes.myprivateblog.data.BlogEntry
+import ar.edu.unq.pdes.myprivateblog.rx.RxSchedulers
+import ar.edu.unq.pdes.myprivateblog.screens.post_create.PostCreateViewModel
+import io.reactivex.Flowable
+import io.reactivex.Single
+import java.io.OutputStreamWriter
+import java.util.*
+import javax.inject.Inject
+
+class BlogEntriesService @Inject constructor(
+    val blogEntriesRepository: BlogEntriesRepository,
+    val context: Context){
+    fun createBlogEntry(title: String, body: String, cardColor: Int): Flowable<Long> {
+        return Flowable.fromCallable {
+
+            createFileWithContent(body)
+
+        }.flatMapSingle {
+
+            saveBlogEntry(title, it, cardColor)
+
+        }.compose(RxSchedulers.flowableAsync())
+
+    }
+
+    private fun saveBlogEntry(
+        title: String,
+        it: String,
+        cardColor: Int
+    ): Single<Long> {
+        return blogEntriesRepository.createBlogEntry(
+            BlogEntry(
+                title = title,
+                bodyPath = it,
+                cardColor = cardColor
+            )
+        )
+    }
+
+    private fun createFileWithContent(body: String): String {
+        val fileName = UUID.randomUUID().toString() + ".body"
+        val outputStreamWriter =
+            OutputStreamWriter(context.openFileOutput(fileName, Context.MODE_PRIVATE))
+        outputStreamWriter.use { it.write(body) }
+        return fileName
+    }
+
+}
