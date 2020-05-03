02f22230d37d51639894e38a1860446f01269ef4
Sebastian Gonzalez <sebastian.gonzalez@fravega.com.ar>
2020-05-02 23:09:53 -0300

    Refactor: post detail view fragment body loading

--- app/src/main/java/ar/edu/unq/pdes/myprivateblog/screens/post_detail/PostDetailViewModel.kt
+++ app/src/main/java/ar/edu/unq/pdes/myprivateblog/screens/post_detail/PostDetailViewModel.kt
@@ -1,28 +1,24 @@
 package ar.edu.unq.pdes.myprivateblog.screens.post_detail
 
-import android.content.Context
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import ar.edu.unq.pdes.myprivateblog.data.BlogEntriesRepository
 import ar.edu.unq.pdes.myprivateblog.data.BlogEntry
 import ar.edu.unq.pdes.myprivateblog.data.EntityID
-import ar.edu.unq.pdes.myprivateblog.rx.RxSchedulers
 import ar.edu.unq.pdes.myprivateblog.services.BlogEntriesService
 import timber.log.Timber
 import javax.inject.Inject
 
 class PostDetailViewModel @Inject constructor(
-    val blogEntriesService: BlogEntriesService,
-    val context: Context
+    private val blogEntriesService: BlogEntriesService
 ) : ViewModel() {
 
     var post = MutableLiveData<BlogEntry?>()
-
-
+    var body = MutableLiveData("")
 
     fun fetchBlogEntry(id: EntityID) {
-        val disposable= blogEntriesService.fetchBlogEntry(id).subscribe {
+        val disposable = blogEntriesService.fetchBlogEntry(id).subscribe {
                 post.value = it
+                body.value = getBody(it)
             }
     }
 
@@ -31,7 +27,6 @@ class PostDetailViewModel @Inject constructor(
             .subscribe {
                 Timber.i("Sarasa")
             }
-
     }
 
     fun getBody(post: BlogEntry): String {
