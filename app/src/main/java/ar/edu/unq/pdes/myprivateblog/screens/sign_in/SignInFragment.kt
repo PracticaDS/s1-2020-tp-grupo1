b9a7ba18580dba60e45ae47a0c00247100602cae
Sebastian Gonzalez <sebastian.gonzalez@fravega.com.ar>
2020-05-16 18:51:41 -0300

    Refactor: normalized sign in fragment

---
+++ app/src/main/java/ar/edu/unq/pdes/myprivateblog/screens/sign_in/SignInFragment.kt
@@ -0,0 +1,9 @@
+package ar.edu.unq.pdes.myprivateblog.screens.sign_in
+
+import ar.edu.unq.pdes.myprivateblog.BaseFragment
+import ar.edu.unq.pdes.myprivateblog.R
+
+class SignInFragment : BaseFragment() {
+    override val layoutId: Int
+        get() = R.layout.fragment_sign_in
+}
