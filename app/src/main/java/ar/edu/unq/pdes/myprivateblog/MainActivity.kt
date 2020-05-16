8031cb6013643a29a1e2995303f35ca4ab09e65b
Sebastian Gonzalez <sebastian.gonzalez@fravega.com.ar>
2020-05-16 18:57:34 -0300

    Refactod: moved sign in code to sign in fragment

--- app/src/main/java/ar/edu/unq/pdes/myprivateblog/MainActivity.kt
+++ app/src/main/java/ar/edu/unq/pdes/myprivateblog/MainActivity.kt
@@ -24,48 +24,9 @@ class MainActivity : DaggerAppCompatActivity() {
     lateinit var viewModelFactory: ViewModelProvider.Factory
 
     val viewModel by viewModels<MainActivityViewModel> { viewModelFactory }
-    private val RC_SIGN_IN = 123
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val auth = FirebaseAuth.getInstance()
-        if (auth.currentUser != null) {
-            // already signed in
-        } else {
-            // not signed in
-        }
-
-        startActivityForResult(
-            AuthUI.getInstance()
-                .createSignInIntentBuilder()
-                .setAvailableProviders(
-                    listOf(GoogleBuilder().build())
-                )
-                .build(),
-            RC_SIGN_IN
-        )
-
-        fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-            super.onActivityResult(requestCode, resultCode, data)
-
-            if (requestCode == RC_SIGN_IN) {
-                val response = IdpResponse.fromResultIntent(data)
-                if (resultCode == Activity.RESULT_OK) {
-                    // Successfully signed in
-                    val user = FirebaseAuth.getInstance().currentUser
-                    //pasar al otro layout
-                } else {
-                    // Sign in failed. If response is null the user canceled the
-                    // sign-in flow using the back button. Otherwise check
-                    // response.getError().getErrorCode() and handle the error.
-                    // ...
-                }
-            }
-        }
-
-
-
 
         RxJavaPlugins.setErrorHandler { Timber.e(it) }
 
