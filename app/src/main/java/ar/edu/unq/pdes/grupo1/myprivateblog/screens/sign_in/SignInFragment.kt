283ee89b8074c4a061117fef5f48551ac31e7a1c
Camila Cintioli <camila.cintioli@gmail.com>
2020-05-24 00:59:49 -0300

    Added sign in button behaviour

--- app/src/main/java/ar/edu/unq/pdes/grupo1/myprivateblog/screens/sign_in/SignInFragment.kt
+++ app/src/main/java/ar/edu/unq/pdes/grupo1/myprivateblog/screens/sign_in/SignInFragment.kt
@@ -2,14 +2,18 @@ package ar.edu.unq.pdes.grupo1.myprivateblog.screens.sign_in
 
 import android.app.Activity
 import android.content.Intent
+import android.opengl.Visibility
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.isVisible
 import androidx.navigation.fragment.findNavController
 import ar.edu.unq.pdes.grupo1.myprivateblog.BaseFragment
 import ar.edu.unq.pdes.grupo1.myprivateblog.R
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.fragment_sign_in.*
+import kotlin.math.sign
 
 class SignInFragment : BaseFragment() {
     override val layoutId: Int
@@ -22,20 +26,27 @@ class SignInFragment : BaseFragment() {
 
         val auth = FirebaseAuth.getInstance()
         if (auth.currentUser != null) {
-            // already signed in
+            navigateToBlogEntriesList()
         } else {
             // not signed in
         }
 
-        startActivityForResult(
-            AuthUI.getInstance()
-                .createSignInIntentBuilder()
-                .setAvailableProviders(
-                    listOf(AuthUI.IdpConfig.GoogleBuilder().build())
-                )
-                .build(),
-            RC_SIGN_IN
-        )
+
+        sign_in_button.setOnClickListener{
+
+            startActivityForResult(
+                AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setAvailableProviders(
+                        listOf(AuthUI.IdpConfig.GoogleBuilder().build())
+                    )
+                    .build(),
+                RC_SIGN_IN
+            )
+
+        }
+
+
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -54,6 +65,18 @@ class SignInFragment : BaseFragment() {
                 // ...
             }
         }
+    }
+
+    private fun signIn(){
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(
+                    listOf(AuthUI.IdpConfig.GoogleBuilder().build())
+                )
+                .build(),
+            RC_SIGN_IN
+        )
     }
 
     private fun navigateToBlogEntriesList() {
