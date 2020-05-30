a4ab1da6f1d4bcd65b708644145152339cf2aecc
Sebastian Gonzalez <sebastian.gonzalez@fravega.com.ar>
2020-05-30 19:22:10 -0300

    Added sync blog entries from firebase

--- app/src/main/java/ar/edu/unq/pdes/grupo1/myprivateblog/services/BlogEntriesService.kt
+++ app/src/main/java/ar/edu/unq/pdes/grupo1/myprivateblog/services/BlogEntriesService.kt
@@ -7,28 +7,34 @@ import ar.edu.unq.pdes.grupo1.myprivateblog.data.BlogEntry
 import ar.edu.unq.pdes.grupo1.myprivateblog.data.EntityID
 import ar.edu.unq.pdes.grupo1.myprivateblog.rx.RxSchedulers
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
 import io.reactivex.Completable
 import io.reactivex.Flowable
 import io.reactivex.Single
 import io.reactivex.internal.operators.single.SingleJust
+import timber.log.Timber
 import java.io.File
 import java.io.OutputStreamWriter
 import java.util.*
 import javax.inject.Inject
+import kotlin.collections.HashMap
 
 class BlogEntriesService @Inject constructor(
     val blogEntriesRepository: BlogEntriesRepository,
     val context: Context){
-    fun createBlogEntry(title: String, body: String, cardColor: Int): Flowable<Long> {
+    fun createBlogEntry(title: String, body: String, cardColor: Int): Flowable<EntityID> {
         return Flowable.fromCallable {
             createFileWithContent(body)
         }.flatMapSingle {
             saveBlogEntry(title, it, cardColor)
         }.flatMapSingle {
-            saveBlogEntryToFirebase(it.toInt(), title, body, cardColor)
-            SingleJust(it)
+            saveBlogEntryToFirebase(it.uid.toInt(), it.title, body, it.bodyPath, it.cardColor)
+            SingleJust(it.uid)
         }.compose(RxSchedulers.flowableAsync())
     }
 
@@ -38,7 +44,7 @@ class BlogEntriesService @Inject constructor(
         }.flatMapSingle {
             updateBlogEntryB(id, title, it, cardColor)
         }.flatMapSingle {
-            saveBlogEntryToFirebase(id, title, body, cardColor)
+            saveBlogEntryToFirebase(id, title, body, bodyPath, cardColor)
             SingleJust(it)
         }.compose(RxSchedulers.flowableAsync())
     }
@@ -69,7 +75,7 @@ class BlogEntriesService @Inject constructor(
         title: String,
         it: String,
         cardColor: Int
-    ): Single<Long> {
+    ): Single<BlogEntry> {
         return blogEntriesRepository.createBlogEntry(
             BlogEntry(
                 title = title,
@@ -98,6 +104,7 @@ class BlogEntriesService @Inject constructor(
     }
 
     fun getAllBlogEntries(): LiveData<List<BlogEntry>> {
+        syncBlogEntriesFromFirebase()
         return blogEntriesRepository.getAllBlogEntries()
     }
 
@@ -105,7 +112,7 @@ class BlogEntriesService @Inject constructor(
         return File(context.filesDir, blogEntry.bodyPath).readText()
     }
 
-    fun saveBlogEntryToFirebase(postId: Int, title: String, body: String, color: Int): Unit {
+    private fun saveBlogEntryToFirebase(postId: Int, title: String, body: String, bodyPath: String, color: Int): Unit {
         val uid = FirebaseAuth.getInstance().currentUser?.uid.toString()
         // Write a message to the database
         val database = Firebase.database
@@ -114,7 +121,38 @@ class BlogEntriesService @Inject constructor(
             "postId" to postId,
             "title" to title,
             "body" to body,
+            "bodyPath" to bodyPath,
             "color" to color
         ))
+    }
+
+    private fun onBlogEntriesReceivedFromFirebase(blogEntries: HashMap<String, HashMap<String, Any>>): Unit {
+        blogEntries.values.forEach {
+            val postId = it["postId"] as Long
+            val title = it["title"] as String
+            val body = it["body"] as String
+            val bodyPath = it["bodyPath"] as String
+            val cardColor = it["color"] as Int
+            updateBlogEntry(postId.toInt(), bodyPath, title, body, cardColor)
+        }
+    }
+
+    private fun syncBlogEntriesFromFirebase() {
+        val uid = FirebaseAuth.getInstance().currentUser?.uid.toString()
+        val database = Firebase.database
+
+        val postListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                dataSnapshot.getValue<HashMap<String, HashMap<String, Any>>>()?.let {
+                    onBlogEntriesReceivedFromFirebase(it)
+                }
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Timber.w("loadPost:onCancelled")
+            }
+        }
+
+        database.getReference("blogEntries/$uid").addListenerForSingleValueEvent(postListener)
     }
 }
